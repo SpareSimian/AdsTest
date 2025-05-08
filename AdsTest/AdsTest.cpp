e24fec5775e37296840e830f8c939af986a400bc
Kenneth Porter <SpareSimian+github@SewingWitch.com>
2025-05-07 19:47:25 -0700

    Stream inserter for axis param set so we can dump it to cout

--- AdsTest/AdsTest.cpp
+++ AdsTest/AdsTest.cpp
@@ -39,6 +39,99 @@ AdsNcConnection::AdsNcConnection()
    addr.port = 500; // NC uses this AMS port
 }
 
+std::ostream& operator<<(std::ostream& os, const TwinCAT3::ST_AxisParameterSet& aps)
+{
+   os << "AxisId: " << aps.AxisId << '\n';
+   os << "nAxisType: " << static_cast<unsigned>(aps.nAxisType) << '\n';
+   os << "sAxisName: " << aps.sAxisName << '\n';
+   os << "fAxisCycleTime: " << aps.fAxisCycleTime << '\n';
+   os << "bEnablePositionAreaControl: " << aps.bEnablePositionAreaControl << '\n';
+   os << "fPositionAreaControlRange: " << aps.fPositionAreaControlRange << '\n';
+   os << "bEnableMotionControl: " << aps.bEnableMotionControl << '\n';
+   os << "fMotionControlTime: " << aps.fMotionControlTime << '\n';
+   os << "bEnableLoop: " << aps.bEnableLoop << '\n';
+   os << "fLoopDistance: " << aps.fLoopDistance << '\n';
+   os << "bEnableTargetPosControl: " << aps.bEnableTargetPosControl << '\n';
+   os << "fTargetPosControlRange: " << aps.fTargetPosControlRange << '\n';
+   os << "fTargetPosControlTime: " << aps.fTargetPosControlTime << '\n';
+   os << "fVeloMaximum: " << aps.fVeloMaximum << '\n';
+   os << "fRefVeloSearch: " << aps.fRefVeloSearch << '\n';
+   os << "fRefVeloSync: " << aps.fRefVeloSync << '\n';
+   os << "fVeloSlowManual: " << aps.fVeloSlowManual << '\n';
+   os << "fVeloFastManual: " << aps.fVeloFastManual << '\n';
+   os << "fMotionControlRange: " << aps.fMotionControlRange << '\n';
+   os << "bEnablePEHTimeControl: " << aps.bEnablePEHTimeControl << '\n';
+   os << "fPEHControlTime: " << aps.fPEHControlTime << '\n';
+   os << "bEnableBacklashCompensation: " << aps.bEnableBacklashCompensation << '\n';
+   os << "fBacklash: " << aps.fBacklash << '\n';
+   os << "sAmsNetId: " << aps.sAmsNetId << '\n';
+   os << "nPort: " << aps.nPort << '\n';
+   os << "nChnNo: " << aps.nChnNo << '\n';
+   os << "fAcceleration: " << aps.fAcceleration << '\n';
+   os << "fDeceleration: " << aps.fDeceleration << '\n';
+   os << "fJerk: " << aps.fJerk << '\n';
+
+   /* ENCODER: */
+   os << "nEncId: " << aps.nEncId << '\n';
+   os << "nEncType: " << static_cast<unsigned>(aps.nEncType) << '\n';
+   os << "sEncName: " << aps.sEncName << '\n';
+   os << "fEncScaleFactorNumerator: " << aps.fEncScaleFactorNumerator << '\n';
+   os << "fEncScaleFactorDenominator: " << aps.fEncScaleFactorDenominator << '\n';
+   os << "fEncScaleFactor: " << aps.fEncScaleFactor << '\n';
+   os << "fEncOffset: " << aps.fEncOffset << '\n';
+   os << "bEncIsInverse: " << aps.bEncIsInverse << '\n';
+   os << "fEncModuloFactor: " << aps.fEncModuloFactor << '\n';
+   os << "nEncMode: " << aps.nEncMode << '\n';
+   os << "bEncEnableSoftEndMinControl: " << aps.bEncEnableSoftEndMinControl << '\n';
+   os << "bEncEnableSoftEndMaxControl: " << aps.bEncEnableSoftEndMaxControl << '\n';
+   os << "fEncSoftEndMin: " << aps.fEncSoftEndMin << '\n';
+   os << "fEncSoftEndMax: " << aps.fEncSoftEndMax << '\n';
+   os << "nEncMaxIncrement: " << aps.nEncMaxIncrement << '\n';
+   os << "nEncRefSoftSyncMask: " << aps.nEncRefSoftSyncMask << '\n';
+   os << "bEncEnablePosCorrection: " << aps.bEncEnablePosCorrection << '\n';
+   os << "nEncReferenceSystem: " << aps.nEncReferenceSystem << '\n';
+   os << "fEncPosCorrectionFilterTime: " << aps.fEncPosCorrectionFilterTime << '\n';
+   os << "bEncRefSearchInverse: " << aps.bEncRefSearchInverse << '\n';
+   os << "bEncRefSyncInverse: " << aps.bEncRefSyncInverse << '\n';
+   os << "nEncRefMode: " << aps.nEncRefMode << '\n';
+   os << "fEncRefPosition: " << aps.fEncRefPosition << '\n';
+
+   /* CONTROLLER: */
+   os << "nCtrlId: " << aps.nCtrlId << '\n';
+   os << "nCtrlType: " << aps.nCtrlType << '\n';
+   os << "sCtrlName: " << aps.sCtrlName << '\n';
+   os << "bCtrlEnablePosDiffControl: " << aps.bCtrlEnablePosDiffControl << '\n';
+   os << "bCtrlEnableVeloDiffControl: " << aps.bCtrlEnableVeloDiffControl << '\n';
+   os << "fCtrlPosDiffMax: " << aps.fCtrlPosDiffMax << '\n';
+   os << "fCtrlPosDiffMaxTime: " << aps.fCtrlPosDiffMaxTime << '\n';
+   os << "fCtrlPosKp: " << aps.fCtrlPosKp << '\n';
+   os << "fCtrlPosTn: " << aps.fCtrlPosTn << '\n';
+   os << "fCtrlPosTv: " << aps.fCtrlPosTv << '\n';
+   os << "fCtrlPosTd: " << aps.fCtrlPosTd << '\n';
+   os << "fCtrlPosExtKp: " << aps.fCtrlPosExtKp << '\n';
+   os << "fCtrlPosExtVelo: " << aps.fCtrlPosExtVelo << '\n';
+   os << "fCtrlAccKa: " << aps.fCtrlAccKa << '\n';
+
+   /* DRIVE: */
+   os << "nDriveId: " << aps.nDriveId << '\n';
+   os << "nDriveType: " << static_cast<unsigned>(aps.nDriveType) << '\n';
+   os << "sDriveName: " << aps.sDriveName << '\n';
+   os << "bDriveIsInverse: " << aps.bDriveIsInverse << '\n';
+   os << "nDriveControlDWord: " << aps.nDriveControlDWord << '\n';
+   os << "fDriveVeloReferenz: " << aps.fDriveVeloReferenz << '\n';
+   os << "fDriveOutputReferenz: " << aps.fDriveOutputReferenz << '\n';
+   os << "fDriveOutputScalingAcc: " << aps.fDriveOutputScalingAcc << '\n';
+   os << "fDriveOutputScalingTorque: " << aps.fDriveOutputScalingTorque << '\n';
+   os << "fDriveInputScalingTorque: " << aps.fDriveInputScalingTorque << '\n';
+   os << "fDriveInputFiltertimeTorque: " << aps.fDriveInputFiltertimeTorque << '\n';
+   os << "fDriveInputFiltertimeTorqueDerivative: " << aps.fDriveInputFiltertimeTorqueDerivative << '\n';
+
+   os << "fAccelerationMax: " << aps.fAccelerationMax << '\n';
+   os << "fDecelerationMax: " << aps.fDecelerationMax << '\n';
+
+   return os;
+};
+
 int main()
 {
    const ads_i32 nTemp = AdsGetDllVersion();
@@ -58,7 +151,8 @@ int main()
    TwinCAT3::ST_AxisParameterSet axisParameterSet;
    nErr = AdsSyncReadReq(&conn.getAddr(), 0x4000 + axisId, axisId * 0x100000, sizeof axisParameterSet, &axisParameterSet);
    if (nErr) throw std::runtime_error(apiFailed("AdsSyncReadReq(ST_AxisParameterSet)", nErr));
-
+   std::cout << axisParameterSet;
+   
    AdsPortClose();
    return 0;
 }
