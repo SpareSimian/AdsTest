b083bee16ad5c5e9b1984e34e9cd32ddb23348b4
Kenneth Porter <SpareSimian+github@SewingWitch.com>
2025-05-07 19:28:52 -0700

    C++ify the connection setup and error handling.

--- AdsTest/AdsTest.cpp
+++ AdsTest/AdsTest.cpp
@@ -1,8 +1,43 @@
 // test ADS communication to TwinCAT 3 NC PTP system
 
 #include <iostream>
+#include <stdexcept>
+#include <sstream>
+
 #include <TcAdsApi.h>
 #include "ST_AxisParameterSet.h"
+
+// RAII class to release port on exceptions
+class AdsPort
+{
+public:
+   AdsPort() { AdsPortOpen(); }
+   ~AdsPort() { AdsPortClose(); }
+};
+
+class AdsNcConnection
+{
+   AdsPort port;
+   AmsAddr addr;
+public:
+   AdsNcConnection();
+   AmsAddr& getAddr() { return addr; }
+};
+
+static std::string apiFailed(const std::string& api, ads_i32 nErr)
+{
+   std::ostringstream os;
+   os << api << " failed: " << nErr;
+   return os.str();
+}
+
+AdsNcConnection::AdsNcConnection()
+{
+   long nErr = AdsGetLocalAddress(&addr);
+   if (nErr)
+      throw std::runtime_error(apiFailed("AdsGetLocalAddress", nErr));
+   addr.port = 500; // NC uses this AMS port
+}
 
 int main()
 {
@@ -12,25 +47,17 @@ int main()
    std::cout << "Revision: " << (int)pDLLVersion->revision << '\n';
    std::cout << "Build: " << pDLLVersion->build << '\n';
 
-   const long nPort = AdsPortOpen();
-   AmsAddr addr;
-   long nErr = AdsGetLocalAddress(&addr);
-   if (nErr)
-   {
-      std::cerr << "AdsGetLocalAddress: " << nErr << '\n';
-      AdsPortClose();
-      return 1;
-   }
-   addr.port = 500; // NC
+   AdsNcConnection conn;
+
    const int axisId = 1;
    ads_i32 axisIdRead;
-   nErr = AdsSyncReadReq(&addr, 0x4000 + axisId, 0x00000001, sizeof axisIdRead, &axisIdRead);
-   if (nErr) std::cerr << "AdsSyncReadReq failed " << nErr << '\n';
+   ads_i32 nErr = AdsSyncReadReq(&conn.getAddr(), 0x4000 + axisId, 0x00000001, sizeof axisIdRead, &axisIdRead);
+   if (nErr) throw std::runtime_error(apiFailed("AdsSyncReadReq", nErr));
    else std::cout << "axis ID read " << axisIdRead << '\n';
 
    TwinCAT3::ST_AxisParameterSet axisParameterSet;
-   nErr = AdsSyncReadReq(&addr, 0x4000 + axisId, axisId * 0x100000, sizeof axisParameterSet, &axisParameterSet);
-   if (nErr) std::cerr << "AdsSyncReadReq(ST_AxisParameterSet) failed " << nErr << '\n';
+   nErr = AdsSyncReadReq(&conn.getAddr(), 0x4000 + axisId, axisId * 0x100000, sizeof axisParameterSet, &axisParameterSet);
+   if (nErr) throw std::runtime_error(apiFailed("AdsSyncReadReq(ST_AxisParameterSet)", nErr));
 
    AdsPortClose();
    return 0;
